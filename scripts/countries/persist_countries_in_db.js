e41099d4b06f28fba7946bf236f3e7547abafb87
LEO LE GOOOOOAAAAAAAAATTTT <leo.menaldo@epita.fr>
2026-05-05 17:09:01 +0200

    feat(countries-script): Finalized script to add every countries to supabase DB

--- scripts/countries/persist_countries_in_db.js
+++ scripts/countries/persist_countries_in_db.js
@@ -1,16 +1,26 @@
 import { createClient } from '@supabase/supabase-js'
 import { loadEnvFile } from 'node:process';
-loadEnvFile('.env');
-const supabase = createClient(process.env.CLIENT_URL, process.env.SUPABASE_PUBLIC_KEY)
+loadEnvFile('../.env');
+const supabase = createClient(process.env.CLIENT_URL, process.env.SERVICE_ROLE)
 
 // countries_table_api = `${process.env.CLIENT_URL}/countries_data`
-const DEBUG=true
+const DEBUG=false
 
 const result = await fetch(`https://restcountries.com/v3.1/${DEBUG ? "name/france" : "all"}?fields=area,borders,name,cca3,idd,capital,translations,flags,tld,unMember`);
-const json = await result.json();
+let json = await result.json();
+if (DEBUG) {
+    json = json.splice(0, 5);
+}
+
+const countriesList = [];
 
 for (let i = 0; i < json.length; i++) {
-    const rawTrans = new Map(Object.entries(json[i]['translations']));
+    const rawTrans = new Map(Object.entries(json[i]["translations"]));
+    const filteredMap = {};
+    rawTrans.forEach((val, key) => {
+        filteredMap[key] = val["common"];
+    });
+    
     const country = {
         flagLink: json[i]["flags"]["png"],
         countryName: json[i]["name"]["common"],
@@ -20,11 +30,16 @@ for (let i = 0; i < json.length; i++) {
         capital: json[i]["capital"],
         borders: json[i]["borders"],
         area: json[i]["area"],
-        idd: json[i]["idd"],
-        translations: rawTrans,
+        iddRoot: json[i]["idd"]["root"],
+        iddSuffixes: json[i]["idd"]["suffixes"],
+        translations: filteredMap,
     }
-    
-    const { data, error } = await supabase.from('countries_data').insert(country);
-    console.log(data);
-    console.log(error);
+
+    countriesList.push(country);
 }
+
+const { data, error } = await supabase.from('countries_data').insert(countriesList);
+console.log({
+    data: data,
+    error: error,
+});
