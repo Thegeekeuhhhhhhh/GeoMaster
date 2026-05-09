649947af5a8a0a3207f0f2e858f3d4b23d662c7b
LEO LE GOOOOOAAAAAAAAATTTT <leo.menaldo@epita.fr>
2026-05-09 21:56:43 +0200

    feat(script): Added a new script to insert CCA2 fields in database

---
+++ scripts/countries/add_fields_to_countries_in_db_2.js
@@ -0,0 +1,37 @@
+import { createClient } from '@supabase/supabase-js'
+import { loadEnvFile } from 'node:process';
+loadEnvFile('../.env');
+const supabase = createClient(process.env.CLIENT_URL, process.env.SERVICE_ROLE)
+
+const DEBUG=false
+const DEBUG2=false
+const BATCH_SIZE = 40;
+
+const result = await fetch(`https://restcountries.com/v3.1/${DEBUG ? "name/france" : "all"}?fields=cca3,cca2`);
+let json = await result.json();
+if (DEBUG2) {
+    json = json.splice(0, 5);
+}
+
+const promises = [];
+
+for (let i = 0; i < json.length; i++) {
+    const country = {
+        cca2: json[i]['cca2'],
+    };
+
+    promises.push(() => supabase.from('countries_data').update(country).eq("cca3", json[i]["cca3"]).then(({data, error}) => {
+        if (error) {
+            console.log({
+                data: data,
+                error: error,
+            });
+        }
+    }));
+}
+
+for (let i = 0; i < promises.length; i += BATCH_SIZE) {
+    const batch = promises.slice(i, i + BATCH_SIZE).map(f => f());
+    await Promise.all(batch);
+    console.log(`BATCH n.${i / BATCH_SIZE + 1} done`);
+}
