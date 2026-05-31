95a29b1c82fc4f090bf6e263b35de36f03a446c2
LEO LE GOAT <leo.menaldo@epita.fr>
2026-05-31 12:57:25 +0200

    feat(scripts): Added script to add custom alternative names to countries

---
+++ scripts/countries/add_fields_to_countries_in_db_3.js
@@ -0,0 +1,71 @@
+import { createClient } from '@supabase/supabase-js'
+import { loadEnvFile } from 'node:process';
+loadEnvFile('../.env');
+const supabase = createClient(process.env.CLIENT_URL, process.env.SERVICE_ROLE)
+
+const BATCH_SIZE = 40;
+const promises = [];
+
+const newAliases = [
+    {
+        cca2: "AG",
+        shortName: "antigua"
+    },
+    {
+        cca2: "BA",
+        shortName: "bosnie"
+    },
+    {
+        cca2: "CD",
+        shortName: "RDC"
+    },
+    {
+        cca2: "MH",
+        shortName: "marshall"
+    },
+    {
+        cca2: "MK",
+        shortName: "macedoine"
+    },
+    {
+        cca2: "VC",
+        shortName: "saint vincent"
+    },
+    {
+        cca2: "ST",
+        shortName: "sao tome"
+    },
+    {
+        cca2: "SB",
+        shortName: "salomon"
+    },
+    {
+        cca2: "TT",
+        shortName: "trinite"
+    },
+    {
+        cca2: "GB",
+        shortName: "angleterre"
+    }
+]
+
+for (let i = 0; i < newAliases.length; i++) {
+    const country = {
+        shortName: newAliases[i]['shortName'],
+    };
+
+    promises.push(() => supabase.from('countries_data').update(country).eq("cca2", newAliases[i]["cca2"]).then(({data, error}) => {
+        if (error) {
+            console.log({
+                data: data,
+                error: error,
+            });
+        }
+    }));
+}
+
+for (let i = 0; i < promises.length; i += BATCH_SIZE) {
+    const batch = promises.slice(i, i + BATCH_SIZE).map(f => f());
+    await Promise.all(batch);
+    console.log(`BATCH n.${i / BATCH_SIZE + 1} done`);
+}
